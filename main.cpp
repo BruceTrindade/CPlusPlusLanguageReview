f2730642e6f97029bfc1f8267d2ed99b437d2392
brucetrindade <brucetrindade2000@gmail.com>
2022-03-04 06:09:35 -0300

    PAUSE repetition workout

--- main.cpp
+++ main.cpp
@@ -4,7 +4,33 @@ using namespace std;
 
 int main () {
 
-    cout << "Hello World" << endl;
+    /*
+        Notepad
+
+        int = um inteiro de 32 bits 
+        long lomng = inteiro de 64 bits
+        float = real de 32bits 
+        double = real de 64bits
+        char = caracter de 32bits
+
+        cout - entrada
+        cin - saida 
+    
+    */
+
+   float a, b, c;
+
+    for (int i = 1; i <= 5; i++) {
+
+
+    cout << "Digite as 3 notas do aluno" << endl;
+    cin >> a >> b >> c;
+
+
+
+    }
+
+
 
     return 0;
 }
